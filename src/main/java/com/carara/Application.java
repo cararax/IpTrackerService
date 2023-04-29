3756edfba406d700404642d625e2f0c7def22d6a
Pedro Carara <cararax@gmail.com>
2023-04-29 19:17:10 -0300

    feat: import controller at main class, set api base path

--- src/main/java/com/carara/Application.java
+++ src/main/java/com/carara/Application.java
@@ -1,16 +1,15 @@
 package com.carara;
 
+import com.carara.controller.IpTrackerController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
-
-import com.carara.controller.PingController;
 
 
 @SpringBootApplication
 // We use direct @Import instead of @ComponentScan to speed up cold starts
 // @ComponentScan(basePackages = "com.carara.controller")
-@Import({ PingController.class })
+@Import({IpTrackerController.class})
 public class Application {
 
     public static void main(String[] args) {
